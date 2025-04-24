ab257934ba07f98252b9a506a692da65f32d8c78
Oleh Levchenko <leva13007@gmail.com>
2025-04-24 20:48:42 +0100

    02 Place mines randomlly

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,26 @@ const grid: Cell[][] = Array(rows).fill(null).map(() => Array(columns).fill(null
   isMine: false,
   isFlagged: false,
   isClicked: false,
-})))
+})));
+
+const createGrid = (): Cell[][] => {
+  const copyGrid = grid.map(row => row.map(cell => ({...cell})));
+  let placedMines = 0;
+  while(mines > placedMines){
+    const r = Math.floor(Math.random() * rows);
+    const c = Math.floor(Math.random() * columns);
+    if (!copyGrid[r][c].isMine) {
+      copyGrid[r][c].isMine = true;
+      placedMines++;
+    }
+  }
+  return copyGrid;
+}
 
 function App() {
-  console.log(grid);
+
+  const board = createGrid()
+  console.log(board);
 
   const leftClickHandler = () => {}
   return (
@@ -28,11 +44,17 @@ function App() {
       </header>
       <main className="main">
         {
-          grid.map((row,i) => (
+          board.map((row,i) => (
             <div key={i} className="board-row">
               {
                 row.map((cell, j) => (
-                  <button onClick={leftClickHandler} key={j} className="board-cell"></button>
+                  <button onClick={leftClickHandler} key={j} className="board-cell">
+                    {
+                      cell.isMine && (
+                        <span>M</span>
+                      )
+                    }
+                  </button>
                 ))
               }
             </div>
